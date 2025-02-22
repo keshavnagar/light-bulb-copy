3b540f1c763914561522839a26f77a3f3c9d345e
keshavnagar <keshavnagar887@gmail.com>
2025-02-22 23:09:01 +0530

    instruction

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const key = document.querySelector(".key")
+const key = document.querySelector(".key");
 const container = document.querySelector(".container");
-key.addEventListener("click",()=>{
-    container.classList.toggle("on");
-})
+const turn = document.querySelector(".turn");
+key.addEventListener("click", () => {
+  container.classList.toggle("on");
 
+  if (container.classList.contains("on")) {
+    turn.innerHTML =
+      '<div>Turn Off<i class="fa-solid fa-arrow-right"></i></div>';
+  } else {
+    turn.innerHTML =
+      '<div>Turn On<i class="fa-solid fa-arrow-right"></i></div>';
+  }
+});
